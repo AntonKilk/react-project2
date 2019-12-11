e9dcc4bb35a69698d72c3161910ecf70ae700cd3
AntonKilk <ant.kilk@gmail.com>
2019-12-11 14:36:26 +0200

    Index changed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 
+let HTML = (props) => {
+return <div>
+            <p>Format: </p>
+            <input name="mode"
+                type="radio" 
+                checked style={{ cursor: "pointer" }}
+                onChange={ () => setFormat("24") }>
+            </input>
+            <span style={{ cursor: "pointer" }} >24</span>
+            <br />
+            <input name="mode"
+                type="radio" 
+                style={{ cursor: "pointer" }}
+                onChange={ () => setFormat("12") }>
+            </input>
+            <span style={{ cursor: "pointer" }} >12</span>
+            <br />
+            </div>  
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let setFormat = (format) => {
+    if ("12") {
+        return console.log("12")
+    } else {
+       return  console.log("24")
+    }
+}
+ 
+class Clock extends React.Component{
 
+    state = {
+        date: new Date(),
+       // format: "24"
+    }
 
+    // handleFormat(){
+    //     this.setState({
+    //         format: setFormat()
+    //     })
+    // }
+
+    componentDidMount(){
+        this.timerID = setInterval (
+            () =>this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timerID)
+    }
+
+    tick () {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render(){
+        return <div>
+        <p>{this.state.date.toLocaleTimeString()}</p>
+        <HTML />
+        </div>
+        
+    }
+}
+
+ReactDOM.render(
+    <Clock />, 
+    document.getElementById('root')
+  )
